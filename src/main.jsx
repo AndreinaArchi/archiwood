059023a174cb5809c420bee59857cd847977a317
Daniele Mazzola <mazzoladaniele@gmail.com>
2025-02-12 22:16:43 +0100

    Feat component Maintenance

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+//import App from './App.jsx'
+import Maintenance from './maintenance/Maintenance.jsx'
+import './assets/Style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Maintenance />
+    {/* <App /> */}
+  </StrictMode>
 )
---
+++ src/maintenance/Maintenance.jsx
@@ -0,0 +1,19 @@
+import './Maintenance.css'
+const Maintenance = () => {
+  return (
+    <div className='maintenance__container'>
+      <div className='maintenance____img'>
+        <img src='/maintenanceImg/logo_maintenance.webp' />
+      </div>
+      <div className='maintenance__message'>
+        <h1>Website Under Maintenance</h1>
+        <div>
+          <p>We are currently performing scheduled maintenance.</p>
+          <p>Please check back later.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Maintenance
