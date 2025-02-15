1037afca1b18cb5d7b5a739603b4b937453fd1de
Daniele Mazzola <mazzoladaniele@gmail.com>
2025-02-15 12:10:38 +0100

    UPDATE styles

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,12 +3,15 @@ import Nav from '../components/Nav/Nav'
 import './Layout.css'
 const Layout = () => {
   return (
-    <>
+    <div className='layout__container'>
       <Nav />
+      <div className='layout__hr'>
+        <hr />
+      </div>
       <section className='layout__section'>
         <Outlet />
       </section>
-    </>
+    </div>
   )
 }
 
