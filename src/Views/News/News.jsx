c4fdeaad6e77686910d9062d4413061b7aeb0640
Daniele Mazzola <mazzoladaniele@gmail.com>
2025-03-23 11:26:26 +0100

    UPDATE News

--- src/Views/News/News.jsx
+++ src/Views/News/News.jsx
@@ -1,20 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import useWidth from '../../hooks/useWidth'
 import { useTranlateNews } from './translateNews'
 import './News.css'
-import SocialChannel from '../../components/SocialChannel/SocialChannel'
-import Button from '../../components/Button/Button'
+import { ScrollContext } from '../../context/createContext'
 
+const SocialChannel = React.lazy(() =>
+  import('../../components/SocialChannel/SocialChannel')
+)
+const Button = React.lazy(() => import('../../components/Button/Button'))
 const Img = React.lazy(() => import('../../components/Img/Img'))
 
 const News = () => {
+  const { SCROLL } = useContext(ScrollContext)
   const width = useWidth()
   const { newsT } = useTranlateNews()
-  console.log(newsT)
+  const prevRef = useRef(null)
 
   const [availableCategories, setAvailableCategories] = useState([])
-  const [allCategoriesNews, setAllCategoriesNews] = useState([])
   const [recentNews, setRecentNews] = useState({})
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   const parseDate = (dateString) => {
     const months = {
@@ -31,8 +35,7 @@ const News = () => {
       Noviembre: 10,
       Diciembre: 11
     }
-
-    const parts = dateString.split(' ') // ["Enero", "18,", "2024"]
+    const parts = dateString.split(' ')
     if (parts.length === 3) {
       const month = months[parts[0]]
       const day = parseInt(parts[1].replace(',', ''), 10)
@@ -44,17 +47,9 @@ const News = () => {
 
   useEffect(() => {
     if (newsT?.news && newsT?.categories) {
-      // Filtrar categorías que tienen al menos una noticia
       const filteredCategories = newsT.categories.filter((category) =>
         newsT.news.some((news) => news.categoryId === category.id)
       )
-
-      // Obtener las primeras 3 noticias de cada categoría
-      const categorizedNews = filteredCategories.flatMap((category) =>
-        newsT.news.filter((news) => news.categoryId === category.id).slice(0, 3)
-      )
-
-      // Obtener la noticia más reciente
       const latestNews =
         newsT.news
           .filter((news) => news.date)
@@ -62,30 +57,10 @@ const News = () => {
           .filter((news) => news.parsedDate)
           .sort((a, b) => b.parsedDate - a.parsedDate)[0] || null
 
-      // Actualizar estados
       setAvailableCategories(filteredCategories)
-      setAllCategoriesNews(categorizedNews)
       setRecentNews(latestNews)
     }
   }, [])
-
-  console.log(availableCategories)
-  console.log(allCategoriesNews)
-  console.log(recentNews)
-
-  /**QUE NECESITO?
-   * 1- En el listado de las categorias, hay que excluir la categoria que aun no disponga de información.
-   * 2- la primera categoria que se seleccionara será por default todas las categorias. Todas las categorias comprende traer de mi array de news
-   * las primeras 2 noticias existente de cada categoria. Considerar que no todas las categorias tendrán por ahora informacion.
-   * 3- En noticia reciente se mostrará la ultima noticia que se haya publicado.
-   * Necesito la logica de esto las const
-   *
-   *
-   *
-   *
-   * */
-
-  console.log(recentNews)
 
   return (
     <section className='news__container fadeIn'>
@@ -99,36 +74,100 @@ const News = () => {
         </div>
         <div className='news__sections'>
           <div className='news__section-news'>
-            <div>
-              {newsT.news?.map((item) => (
-                <div key={item.id} className='news__section-news-content'>
-                  {item.video && (
-                    <iframe
-                      width='100%'
-                      height='500px'
-                      src={item.video}
-                      title='YouTube video player'
-                      frameBorder='0'
-                      allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-                      referrerPolicy='strict-origin-when-cross-origin'
-                      allowFullScreen
-                    ></iframe>
-                  )}
-                  {item.image && <Img img={item.image} w='100%' h='auto' />}
-                  <div className='news__section-news-title'>
-                    <h2>{item.title || 'Título no disponible'}</h2>
-                    <p>{`${item.date || 'Fecha no disponible'} | ${
-                      item.category || 'Categoría no disponible'
-                    }`}</p>
+            <div ref={prevRef}>
+              {newsT.news?.length > 0 && (
+                <>
+                  <div
+                    key={newsT.news[currentIndex].id}
+                    className='news__section-news-content'
+                  >
+                    {newsT.news[currentIndex].video && (
+                      <iframe
+                        width='100%'
+                        height='500px'
+                        src={newsT.news[currentIndex].video}
+                        title='YouTube video player'
+                        frameBorder='0'
+                        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                        referrerPolicy='strict-origin-when-cross-origin'
+                        allowFullScreen
+                      ></iframe>
+                    )}
+                    {newsT.news[currentIndex].image && (
+                      <Img
+                        img={newsT.news[currentIndex].image}
+                        w='100%'
+                        h='auto'
+                      />
+                    )}
+                    <div className='news__section-news-title'>
+                      <h2>
+                        {newsT.news[currentIndex].title ||
+                          'Título no disponible'}
+                      </h2>
+                      <p>{`${
+                        newsT.news[currentIndex].date || 'Fecha no disponible'
+                      } | ${
+                        newsT.news[currentIndex].category ||
+                        'Categoría no disponible'
+                      }`}</p>
+                    </div>
+                    <article>
+                      <p style={{ whiteSpace: 'pre-line' }}>
+                        {newsT.news[currentIndex].content ||
+                          'Contenido no disponible.'}
+                      </p>
+                    </article>
+                    {/* Lista de noticias */}
+                    {newsT.news?.map((item, index) => (
+                      <React.Fragment key={item.id}>
+                        <hr className='separete__news' />
+                        <div className='news__list-news-content'>
+                          {item.video && (
+                            <iframe
+                              width='250px'
+                              height='170px'
+                              src={item.video}
+                              title='YouTube video player'
+                              frameBorder='0'
+                              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                              referrerPolicy='strict-origin-when-cross-origin'
+                              allowFullScreen
+                            ></iframe>
+                          )}
+                          {item.image && (
+                            <Img img={item.image} w='250px' h='170px' />
+                          )}
+                          <div className='news__list-news-title'>
+                            <p>
+                              {item.date} | {item.category}
+                            </p>
+                            <h3>{item.title || 'Título no disponible'}</h3>
+                            <article>
+                              <p>
+                                {item.content.slice(0, 120) ||
+                                  'Contenido no disponible.'}
+                                ...
+                              </p>
+                            </article>
+                            <Button
+                              value={newsT?.texts?.btnText || 'Ver más'}
+                              txtColor='var(--aw-text-btn)'
+                              bgColor='var(--aw-bg-thir)'
+                              p='5px 10px'
+                              br='0px'
+                              action={() => {
+                                setCurrentIndex(index)
+                                SCROLL(prevRef)
+                              }}
+                            />
+                          </div>
+                        </div>
+                      </React.Fragment>
+                    ))}
                   </div>
-
-                  <article>
-                    <p style={{ whiteSpace: 'pre-line' }}>
-                      {item.content || 'Contenido no disponible.'}
-                    </p>
-                  </article>
-                </div>
-              ))}
+                </>
+              )}
             </div>
           </div>
           <div className='news__section-categories'>
@@ -155,31 +194,47 @@ const News = () => {
               <h3>{newsT.texts?.recentNews}</h3>
               <hr />
               <div>
-                {/**RECENT NEWS recentNews*/}
-                <div
-                  key={recentNews?.id}
-                  className='news__recent-news-content'
-                >
-                  {recentNews?.video && (
-                    <iframe
-                      width='100%'
-                      height='220px'
-                      src={recentNews.video}
-                      title='YouTube video player'
-                      frameBorder='0'
-                      allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-                      referrerPolicy='strict-origin-when-cross-origin'
-                      allowFullScreen
-                    ></iframe>
-                  )}
-                  {recentNews.image && (
-                    <Img img={recentNews.image} w='100%' h='auto' />
-                  )}
-                  <div>
-                    <h3>{recentNews.title || 'Título no disponible'}</h3>
+                {recentNews?.id && (
+                  <div key={recentNews?.id} className='news__recent-news-content'>
+                    {recentNews?.video && (
+                      <iframe
+                        width='100%'
+                        height='220px'
+                        src={recentNews.video}
+                        title='YouTube video player'
+                        frameBorder='0'
+                        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                        referrerPolicy='strict-origin-when-cross-origin'
+                        allowFullScreen
+                      ></iframe>
+                    )}
+                    {recentNews.image && (
+                      <Img img={recentNews.image} w='100%' h='auto' />
+                    )}
+                    <div className='news__list-news-title'>
+                      <p>
+                        {recentNews.date} | {recentNews.category}
+                      </p>
+                      <h3>{recentNews.title || 'Título no disponible'}</h3>
+                      <article>
+                        <p>
+                          {recentNews.content.slice(0, 120) ||
+                            'Contenido no disponible.'}
+                          ...
+                        </p>
+                      </article>
+                    </div>
+                    <Button
+                      value={newsT?.texts?.btnText}
+                      txtColor='var(--aw-text-btn)'
+                      bgColor='var(--aw-bg-thir)'
+                      p='5px 10px'
+                      br='0px'
+                    />
                   </div>
-                </div>
+                )}
               </div>
+              {width <= 956 && <hr style={{ marginTop: '30px' }} />}
             </div>
             <div className='news__social'>
               <h3>{newsT.texts?.follow}</h3>
