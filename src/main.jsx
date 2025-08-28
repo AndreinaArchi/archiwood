46749bf134d2f8abaf7567db0f091d9b0b551d1d
Daniele Mazzola <mazzoladaniele@gmail.com>
2025-08-28 12:50:03 +0200

    Maintance add

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import i18next from './locales/config/i18n'
 import { I18nextProvider } from 'react-i18next'
-//import Maintenance from './maintenance/Maintenance.jsx'
+import Maintenance from './maintenance/Maintenance.jsx'
 import './assets/Style.css'
 import { ScrollProvider } from './context/Scroll.jsx'
 
@@ -12,10 +12,10 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <I18nextProvider i18n={i18next}>
-        {/*<Maintenance />*/}
-        <ScrollProvider>
+        <Maintenance />
+        {/* <ScrollProvider>
           <App />
-        </ScrollProvider>
+        </ScrollProvider> */}
       </I18nextProvider>
     </BrowserRouter>
   </StrictMode>
