2835d1eb169b42c8553dcc545ba3d080a27a1f49
Daniele Mazzola <mazzoladaniele@gmail.com>
2025-08-28 12:56:10 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+//import App from './App.jsx'
 import i18next from './locales/config/i18n'
 import { I18nextProvider } from 'react-i18next'
 import Maintenance from './maintenance/Maintenance.jsx'
 import './assets/Style.css'
-import { ScrollProvider } from './context/Scroll.jsx'
+//import { ScrollProvider } from './context/Scroll.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
